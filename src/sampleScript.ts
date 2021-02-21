01292a838c5abc5396828d35619becbd7be966a5
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-02-21 16:34:22 +0530

    added demo for tags

--- src/sampleScript.ts
+++ src/sampleScript.ts
@@ -1,2 +1,14 @@
 // remove semicolom and it will be added again and all other fetaures you can try here
 console.info('Hello there, You can put your stuff inside src');
+
+function demoFunction(){
+  // demo tags
+  // TODO - demo todo
+  // HACK - demo hack tag
+  // BUG - dmeo bug tag
+  // FIXME - dmeo FIXME tag
+  // does nothing
+  return 'nothing'
+}
+
+if (require.main?.filename === __filename) demoFunction()
